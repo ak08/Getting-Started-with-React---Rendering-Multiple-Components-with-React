f5a5b91e7704a63339b83ffc540e88f62e270202
abhikaushik <abhikaushik@paypal.com>
2021-05-14 03:02:30 +0530

    added heading and description

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,10 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
+    <div id="main">
+      <h1 data-ns-test="project-name">Twitter</h1>
+      <p data-ns-test="project-description">Allows us to send short messages</p>
+    </div>
   )
 }
 
